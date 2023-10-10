dcb8b56c522323f6ab2efc7dfd90534f6c033315
Jan Hernas <hernasj@wp.pl>
2023-10-10 10:39:09 +0200

    Add bridge for multiple robots

--- leo_gz_bringup/launch/leo_gz.launch.py
+++ leo_gz_bringup/launch/leo_gz.launch.py
@@ -17,7 +17,6 @@ def generate_launch_description():
     pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
     pkg_project_gazebo = get_package_share_directory("leo_gz_bringup")
     pkg_project_worlds = get_package_share_directory("leo_gz_worlds")
-    pkg_project_description = get_package_share_directory("leo_description")
 
     sim_world = DeclareLaunchArgument(
         "sim_world",
@@ -39,52 +38,10 @@ def generate_launch_description():
         ),
     )
 
-    robot_desc = xacro.process(
-        os.path.join(pkg_project_description, "urdf", "leo_sim.urdf.xacro")
-    )
-
-    # Takes the description and joint angles as inputs and publishes the 3D poses of the robot links
-    robot_state_publisher = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        name="robot_state_publisher",
-        output="both",
-        parameters=[
-            {"use_sim_time": True},
-            {"robot_description": robot_desc},
-        ],
-    )
-
-    # Bridge ROS topics and Gazebo messages for establishing communication
-    topic_bridge = Node(
-        package="ros_gz_bridge",
-        executable="parameter_bridge",
-        parameters=[
-            {
-                "config_file": os.path.join(
-                    pkg_project_gazebo, "config", "ros_gz_bridge.yaml"
-                ),
-                "qos_overrides./tf_static.publisher.durability": "transient_local",
-            }
-        ],
-        output="screen",
-    )
-
-    # Camera image bridge
-    image_bridge = Node(
-        package="ros_gz_image",
-        executable="image_bridge",
-        arguments=["/camera/image_raw"],
-        output="screen",
-    )
-
     return LaunchDescription(
         [
             sim_world,
             gz_sim,
             robot_sim,
-            robot_state_publisher,
-            topic_bridge,
-            image_bridge,
         ]
     )
--- leo_gz_bringup/launch/spawn_robot.launch.py
+++ leo_gz_bringup/launch/spawn_robot.launch.py
@@ -1,11 +1,52 @@
+import os
+import xacro
+
 from ament_index_python.packages import get_package_share_directory
 
-from launch import LaunchDescription
-
+from launch import LaunchDescription, LaunchContext
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
 
 
-def generate_launch_description():
+def spawn_robot(context: LaunchContext, namespace: LaunchConfiguration):
+    pkg_project_description = get_package_share_directory("leo_description")
+    robot_name = context.perform_substitution(namespace)
+
+    robot_desc = xacro.process(
+        os.path.join(
+            pkg_project_description,
+            "urdf",
+            "leo_sim.urdf.xacro",
+        ),
+        mappings={"robot_ns": robot_name},
+    )
+
+    if robot_name == "":
+        robot_state_publisher_node_name = "robot_state_publisher"
+        robot_gazebo_name = "leo_rover"
+        topic_bridge_node_name = "topic_bridge"
+        image_bridge_node_name = "image_bridge"
+    else:
+        robot_state_publisher_node_name = robot_name + "_robot_state_publisher"
+        robot_gazebo_name = "leo_rover_" + robot_name
+        topic_bridge_node_name = robot_name + "_topic_bridge"
+        image_bridge_node_name = robot_name + "_image_bridge"
+
+    # Takes the description and joint angles as inputs and publishes the 3D poses of the robot links
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name=robot_state_publisher_node_name,
+        output="both",
+        parameters=[
+            {"use_sim_time": True},
+            {"robot_description": robot_desc},
+        ],
+        remappings=[
+            ("/robot_description", robot_name + "/robot_description"),
+        ],
+    )
     # Spawn a robot inside a simulation
     leo_rover = Node(
         package="ros_gz_sim",
@@ -14,16 +55,70 @@ def generate_launch_description():
         output="both",
         arguments=[
             "-topic",
-            "robot_description",
+            robot_name + "/robot_description",
             "-name",
-            "leo_rover",
+            robot_gazebo_name,
             "-z",
             "1.65",
         ],
     )
 
+    # Bridge ROS topics and Gazebo messages for establishing communication
+    topic_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        name=topic_bridge_node_name,
+        arguments=[
+            "/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock",
+            robot_name + "/cmd_vel@geometry_msgs/msg/Twist]ignition.msgs.Twist",
+            robot_name + "/odom@nav_msgs/msg/Odometry[ignition.msgs.Odometry",
+            robot_name + "/tf@tf2_msgs/msg/TFMessage[ignition.msgs.Pose_V",
+            robot_name + "/imu/data_raw@sensor_msgs/msg/Imu[ignition.msgs.IMU",
+            robot_name
+            + "/camera/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo",
+            "world/leo_marsyard/model/"
+            + robot_gazebo_name
+            + "/joint_state@sensor_msgs/msg/JointState[ignition.msgs.Model",
+        ],
+        parameters=[
+            {
+                "qos_overrides./tf_static.publisher.durability": "transient_local",
+            }
+        ],
+        remappings=[
+            (
+                "world/leo_marsyard/model/" + robot_gazebo_name + "/joint_state",
+                robot_name + "/joint_states",
+            ),
+        ],
+        output="screen",
+    )
+
+    # Camera image bridge
+    image_bridge = Node(
+        package="ros_gz_image",
+        executable="image_bridge",
+        name=image_bridge_node_name,
+        arguments=[robot_name + "/camera/image_raw"],
+        output="screen",
+    )
+    return [
+        robot_state_publisher,
+        leo_rover,
+        topic_bridge,
+        image_bridge,
+    ]
+
+
+def generate_launch_description():
+    name_argument = DeclareLaunchArgument(
+        "robot_name",
+        default_value="",
+        description="Robot namespace",
+    )
+
+    namespace = LaunchConfiguration("robot_name")
+
     return LaunchDescription(
-        [
-            leo_rover,
-        ]
+        [name_argument, OpaqueFunction(function=spawn_robot, args=[namespace])]
     )
