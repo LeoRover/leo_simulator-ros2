0d655f95d9fe5469d22ee111ffbef730d8f2e72d
Błażej Sowa <bsowa123@gmail.com>
2024-11-08 17:08:47 +0100

    Fix mypy errors (#11)
    (cherry picked from commit 295b5e458c02fdca55a0260f024e78ca5aa1cf1f)

--- leo_gz_bringup/launch/leo_gz.launch.py
+++ leo_gz_bringup/launch/leo_gz.launch.py
@@ -23,9 +23,9 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
+from launch.launch_description import LaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
